cbfd5344886249b5a74f5f5669040af7d58bafa7
JeongKiKi <jkh001301@gmail.com>
2024-04-11 03:11:41 +0900

    feat-tabBar 추가

---
+++ WordMemory/MemoryListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryListViewController.swift
+//  WordMemory
+//
+//  Created by 정기현 on 2024/04/11.
+//
+
+import UIKit
+
+class MemoryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = . yellow
+    }
+  
+}
---
+++ WordMemory/MemoryTestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryTestViewController.swift
+//  WordMemory
+//
+//  Created by 정기현 on 2024/04/11.
+//
+
+import UIKit
+
+class MemoryTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+  
+}
