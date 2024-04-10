b366cfeae8364b95247c73da5f6f5d07da5c79e8
JeongKiKi <jkh001301@naver.com>
2024-04-10 14:13:33 +0900

    Initial Commit

---
+++ WordMemory/MainPage/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordMemory
+//
+//  Created by 정기현 on 2024/04/09.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WordMemory/MainPage/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  WordMemory
+//
+//  Created by 정기현 on 2024/04/10.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
