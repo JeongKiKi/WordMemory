27c46ec9e1271813cbb99085152cc27c865818d0
JeongKiKi <jkh001301@gmail.com>
2024-05-16 21:22:09 +0900

    fix-mainpage mvc변경

--- WordMemory/Controller/MainViewController.swift
+++ WordMemory/Controller/MainViewController.swift
@@ -8,12 +8,21 @@
 import MLKitTranslate
 import UIKit
 class MainViewController: UIViewController {
+    let mainFunc = MainFunc()
     let mainView = MainView()
+    
     var koreanEnglishTranslator: Translator?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "검색"
+        // searchButton 추가
         mainView.searchButton.addTarget(self, action: #selector(MainViewController.searchButtonTapped), for: .touchUpInside)
+    }
+
+    override func loadView() {
+        super.loadView()
+        view = mainView
     }
 
     // 화면클릭시 키보드 내려감
@@ -21,66 +30,13 @@ class MainViewController: UIViewController {
         view.endEditing(true)
     }
 
-    override func loadView() {
-        super.loadView()
-        view.addSubview(mainView)
-        mainView.snp.makeConstraints {
-            $0.top.equalTo(self.view)
-            $0.leading.bottom.trailing.equalToSuperview()
-        }
-    }
-
+    // searchButton 클릭시 실행되는 함수
     @objc func searchButtonTapped() {
         guard let text = mainView.inputWord.text else { return }
-        if text.containsKorean() {
-            changeLanguageKoreanToEnglish()
-        } else {
-            changeLanguageEnglishToKorean()
-        }
-
-        koreanEnglishTranslator?.translate(text) { translatedText, error in
-            guard error == nil, let translatedText = translatedText else { return }
-            print("Translated text:", translatedText)
+        // 언어 확인 후 언어모델 다운 후 변역
+        mainFunc.languageClassiFication(text: text) { translatedText in
+            print("result text:", translatedText)
             self.mainView.outputWord.text = translatedText
         }
-    }
-
-    func changeLanguageKoreanToEnglish() {
-        koreanEnglishTranslator = Translator.translator(options: TranslatorOptions(sourceLanguage: .korean, targetLanguage: .english))
-        let conditions = ModelDownloadConditions(
-            allowsCellularAccess: false, // 셀룰러 허용 여부
-            allowsBackgroundDownloading: true // 백그라운드 다운 허용 여부
-        )
-        koreanEnglishTranslator?.downloadModelIfNeeded(with: conditions) { error in
-            guard error == nil else { return print("언어모델 다운 실패") }
-            print("언어 모델 다운로드 완료")
-        }
-    }
-
-    func changeLanguageEnglishToKorean() {
-        koreanEnglishTranslator = Translator.translator(options: TranslatorOptions(sourceLanguage: .english, targetLanguage: .korean))
-        let conditions = ModelDownloadConditions(
-            allowsCellularAccess: false, // 셀룰러 허용 여부
-            allowsBackgroundDownloading: true // 백그라운드 다운 허용 여부
-        )
-        koreanEnglishTranslator?.downloadModelIfNeeded(with: conditions) { error in
-            guard error == nil else { return print("언어모델 다운 실패") }
-            print("언어 모델 다운로드 완료")
-        }
-    }
-}
-
-extension String {
-    //한글인지 판단하는 함수 추가 (한글이면 true 아니면 false)
-    func containsKorean() -> Bool {
-        for scalar in unicodeScalars {
-            // 한글 유니코드 범위: 0xAC00 ~ 0xD7A3
-            if scalar.value >= 0xAC00, scalar.value <= 0xD7A3 {
-                return true
-            } else {
-                return false
-            }
-        }
-        return false
     }
 }
---
+++ WordMemory/Model/MainFunc.swift
@@ -0,0 +1,67 @@
+//
+//  MainFunc.swift
+//  WordMemory
+//
+//  Created by 정기현 on 2024/05/16.
+//
+
+import Foundation
+import MLKitTranslate
+
+class MainFunc {
+    var koreanEnglishTranslator: Translator?
+    // 구글번역 언어 설정 한국어 -> 영어
+    private func changeLanguageKoreanToEnglish() {
+        koreanEnglishTranslator = Translator.translator(options: TranslatorOptions(sourceLanguage: .korean, targetLanguage: .english))
+        let conditions = ModelDownloadConditions(
+            allowsCellularAccess: false, // 셀룰러 허용 여부
+            allowsBackgroundDownloading: true // 백그라운드 다운 허용 여부
+        )
+        koreanEnglishTranslator?.downloadModelIfNeeded(with: conditions) { error in
+            guard error == nil else { return print("언어모델 다운 실패") }
+            print("언어 모델 다운로드 완료")
+        }
+    }
+
+    // 구글번역 언어 설정 영어 -> 한국어
+    private func changeLanguageEnglishToKorean() {
+        koreanEnglishTranslator = Translator.translator(options: TranslatorOptions(sourceLanguage: .english, targetLanguage: .korean))
+        let conditions = ModelDownloadConditions(
+            allowsCellularAccess: false, // 셀룰러 허용 여부
+            allowsBackgroundDownloading: true // 백그라운드 다운 허용 여부
+        )
+        koreanEnglishTranslator?.downloadModelIfNeeded(with: conditions) { error in
+            guard error == nil else { return print("언어모델 다운 실패") }
+            print("언어 모델 다운로드 완료")
+        }
+    }
+
+    // 언어 구별(한글인지 영어인지) -> 언어모델 다운
+    func languageClassiFication(text: String, completion: @escaping (String) -> Void){
+        if text.containsKorean() {
+            changeLanguageKoreanToEnglish()
+        } else {
+            changeLanguageEnglishToKorean()
+        }
+        koreanEnglishTranslator?.translate(text) { translatedText, error in
+            guard error == nil, let translatedText = translatedText else { return }
+            print("Translated text:", translatedText)
+            completion(translatedText)
+        }
+    }
+}
+
+extension String {
+    // 한글인지 판단하는 함수 추가 (한글이면 true 아니면 false)
+    func containsKorean() -> Bool {
+        for scalar in unicodeScalars {
+            // 한글 유니코드 범위: 0xAC00 ~ 0xD7A3
+            if scalar.value >= 0xAC00, scalar.value <= 0xD7A3 {
+                return true
+            } else {
+                return false
+            }
+        }
+        return false
+    }
+}
